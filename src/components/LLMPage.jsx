3f5df0b16517d8f78abc646161a3a3ea1e55bf85
yashwanth-kadiyam <yashwanth.kadiyam-osi@hivenet.com>
2024-06-12 22:45:24 +0530

    (chore): changed the backend URL

--- src/components/LLMPage.jsx
+++ src/components/LLMPage.jsx
@@ -3,8 +3,8 @@ import ChatWindow from "./ChatWindow";
 
 
 function LLMPage({ setUrl, messages, sendMessage, url }) {
-  if (!url.includes(`http://192.168.34.15:8005/api/v1/query/1/`)) {
-    setUrl("http://192.168.34.15:8005/api/v1/query/1/");
+  if (!url.includes(`http://192.168.34.30:8005/api/v1/query/1/`)) {
+    setUrl("http://192.168.34.30:8005/api/v1/query/1/");
   }
   return (
     <div className="w-full">
